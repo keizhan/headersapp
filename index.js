36505863993a86e8553b0039472861c7f8557eaa
adal_sergio <44279760+Adal8819@users.noreply.github.com>
2025-03-04 14:19:57 -0600

    Update index.js
    change port to 80

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url)) // this is to handle f
 
 
 const app = express();
-const port = 443;
+const port = 80;
 
 // My own middleware 
 function logger(req, res, next) {
